04b7d8d92237999e730d93bb4b3d820af450527e
Ekaterina Sviridova <sviridova.1996@gmail.com>
2026-03-12 15:46:43 +0100

    gen dev short gpt

--- configs/oar_config_litellm.sh
+++ configs/oar_config_litellm.sh
@@ -1,4 +1,4 @@
-NAME="ann_short_gen_gpt4o"
+NAME="ann_short_gen_gpt5"
 PROJECT_NAME="test1"
 HOME="/home/esvirido"
 PROJECT_DIR="$HOME/phd/test1"
@@ -28,11 +28,11 @@ OAR_OUT=$(oarsub \
     "export OPENAI_API_KEY=$OPENAI_API_KEY; \
      module load conda; \
      source /home/esvirido/miniconda3/bin/activate /home/esvirido/miniconda3/envs/llm-env; \
-     echo 'Starting implicitness reconstruction with gpt4o...'; \
+     echo 'Starting implicitness reconstruction with gpt5...'; \
      python3 scripts/generation/ann_short_gen_gemini.py \
         --input_file data/processed/speaker_exchanges_dev_labeled_20260302_101921.jsonl \
         --output_dir results_dev/ann_short_reconstructed_litellm \
-        --model 'openai/gpt-4o' \
+        --model 'openai/gpt-5' \
         --temperature 0.0; \
      echo 'LiteLLM text reconstruction completed.'
     " \
